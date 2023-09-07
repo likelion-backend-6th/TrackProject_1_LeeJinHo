6d4accca4187cb0727d622c55d053ad618f1b9c7
bainaryho <rkswl233@naver.com>
2023-09-07 17:56:04 +0900

    add) first migrations

--- content/models.py
+++ content/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    content = models.TextField(max_length=300)
+    user_id = models.TextField()
+    like_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
