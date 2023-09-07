0eb27cb82fc81c8dce5d3f0143f8f2a696799986
bainaryho <rkswl233@naver.com>
2023-09-08 00:41:09 +0900

    init) user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+    user_id = models.TextField(max_length=24)
+    email = models.EmailField(unique=True, default="example@abc.com")
+    USERNAME_FIELD = "email"
+
+    class Meta:
+        db_table = "User"
