5b121fa3eb41c96549177714963e9e41560f7222
bainaryho <rkswl233@naver.com>
2023-09-08 17:56:25 +0900

    FEAT) Fix runserver error

--- scripts/run_gunicorn.sh
+++ scripts/run_gunicorn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 실행 경로 진입
-cd /home/lion/lion_django_app/lion_app
+cd /home/lion/twitter/twitter
 # activate venv
-source /home/lion/lion_django_app/venv/bin/activate
+source /home/lion/twitter/venv/bin/activate
 # gunicorn 실행
-gunicorn lion_app.wsgi:application --config lion_app/gunicorn_config.py
+gunicorn twitter.wsgi:application --config twitter/gunicorn_config.py
